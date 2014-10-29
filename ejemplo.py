dab6bd212223e3416c881ae1855e6b226317935d
maycck <angelk61@hotmail.com>
2014-10-29 17:30:52 -0600

    ejemplo

---
+++ ejemplo.py
@@ -0,0 +1,17 @@
+def sumaDos():
+    print 10 * 20
+    
+def division(a, b):
+    result = a/b
+    print result
+    
+def msj():
+    print "El resultado es"
+    
+def main():
+    msj()
+    sumaDos()
+    division(300.54,2100.0)
+
+if __name__ == "__main__":
+    main()
