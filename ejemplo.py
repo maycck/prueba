65e2afb6a1a1dfc73040cc9ada73f1f01900394d
maycck <angelk61@hotmail.com>
2014-10-29 18:59:16 -0600

    modific

--- ejemplo.py
+++ ejemplo.py
@@ -1,3 +1,5 @@
+import urllib2
+
 def sumaDos():
     print 10 * 20
     
@@ -7,11 +9,43 @@ def division(a, b):
     
 def msj():
     print "El resultado es"
+
+class Estudiante(object):
+    def __init__(self, nombre, edad):
+        self.nombre = nombre
+        self.edad = edad
+
+    def hola(self):
+        return self.nombre
     
+    def esMayor(self):
+        if self.edad >= 18:
+            return true
+        else:
+            return false
+def cast():
+    lista = [1,2,3,"hola",{"key1":"lista1", "key2":"lista2"}, (1,2,3)]
+    tupla = (1,2,3)
+    diccionario = {"key1":"Miki", "key2":"Pepe","key3":"Daniel"}
+    lista.append("hello")
+    for k,v in diccionario.items():
+        print "%s %s" % (k,v)
+
+    for i in lista:
+        print i
+
+def getweb():
+    try:
+        web = urllib2.urlopen("http://itjiquilpan.edu.mx/")
+        print web.read()
+        web.close()
+    except urllib2.HTTPError, e:
+        print e
+    except urllib2.URLError as e:
+        print e
+
 def main():
-    msj()
-    sumaDos()
-    division(300.54,2100.0)
+    cast()
 
 if __name__ == "__main__":
     main()
